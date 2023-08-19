14d70e49c9b0ce90ee075a0c17458f75bf43e89a
Kangmin <ehfehf0629@gmail.com>
2023-08-20 03:33:15 +0900

    test: BDDMockito

--- cafe-kiosk/src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ cafe-kiosk/src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -3,6 +3,7 @@ package sample.cafekiosk.spring.api.service.mail;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -23,17 +24,18 @@ import static org.mockito.Mockito.when;
 /**
  * mockito 사용해서 mock 객체를 만들 때
  * @Mock 동작
+ * 안 쓰면 NullPointerException
  */
 @ExtendWith(MockitoExtension.class)
 class MailServiceTest {
 
-    //@Mock
+    @Mock
     /**
      * @Spy는 when절 사용불가
      * @Spy는 실제 객체를 기반으로 만들어짐
      * 실제 객체를 mocking 하려고 하기 때문에 stubbing이 되지 않음
      */
-    @Spy
+    //@Spy
     private MailSendClient mailSendClient;
 
     @Mock
@@ -50,16 +52,25 @@ class MailServiceTest {
 //        MailSendHistoryRepository mailSendHistoryRepository = mock(MailSendHistoryRepository.class);
 //        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
 
-//        when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+        /**
+         * 모든 동작 동일
+         * 메서드 이름만 다름
+         * bdd 스타일
+         * given/when/then 에 맞춰서 작성 가능
+         * mockito 사용할 때 BDDMockito 를 쓰자
+         */
+//        Mockito.when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
 //                .thenReturn(true);
+        BDDMockito.given(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+                .willReturn(true);
 
         /**
          * mailSendClient의 sendEmail만 stubbing된거고 남은 진짜 객체의 기능은 그대로 동작
          * 한 객체에서 일부는 실제 객체를 사용하고 싶고 나머지 일부만 Stubbing을 하고 싶을 때 @Spy 사용
          */
-        doReturn(true)
-                .when(mailSendClient)
-                .sendEmail(anyString(), anyString(), anyString(), anyString());
+//        doReturn(true)
+//                .when(mailSendClient)
+//                .sendEmail(anyString(), anyString(), anyString(), anyString());
 
         // when
         boolean result = mailService.sendMail("", "", "", "");
