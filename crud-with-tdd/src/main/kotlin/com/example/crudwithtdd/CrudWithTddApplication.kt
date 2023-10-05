9a18c6548e79c24326af362366a2afe83dcf6fbb
alsdl0629 <ehfehf0629@gmail.com>
2023-10-05 10:15:41 +0900

    Init: Crud With TDD Project

---
+++ crud-with-tdd/src/main/kotlin/com/example/crudwithtdd/CrudWithTddApplication.kt
@@ -0,0 +1,11 @@
+package com.example.crudwithtdd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudWithTddApplication
+
+fun main(args: Array<String>) {
+    runApplication<CrudWithTddApplication>(*args)
+}
---
+++ crud-with-tdd/src/test/kotlin/com/example/crudwithtdd/CrudWithTddApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.crudwithtdd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrudWithTddApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
